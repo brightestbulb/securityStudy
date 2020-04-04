e1dddc9dfa234076c4712415220e364a3a08b9fa
brightestbulb <gisun9021@naver.com>
2020-04-04 16:04:22 +0900

    Initial commit

---
+++ src/test/java/io/security/securitystudy/SecuritystudyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.securitystudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritystudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
