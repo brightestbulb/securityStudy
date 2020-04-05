2d863333ddf90d1c8465150f9acae1b12e9d9dac
brightestbulb <gisun9021@naver.com>
2020-04-05 16:41:06 +0900

    SecurityConfig

--- src/main/java/io/security/securitystudy/SecurityConfig.java
+++ src/main/java/io/security/securitystudy/SecurityConfig.java
@@ -4,6 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity  // Security 관련 여러 클래스들을 설정(= 웹보안 활성화)
@@ -15,6 +24,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .anyRequest().authenticated();  // 어떤 요청에도 인증을 받는다(= 인가정책)
         http  // 인증
-                .formLogin();
+                .formLogin()
+                .loginPage("/loginPage")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login")
+                .usernameParameter("userId")
+                .passwordParameter("passwd")
+                .loginProcessingUrl("/login_proc")
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override  //authentication : 인증에 성공했을때 받는 인증 객체
+                    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                        System.out.println("authentication : " + authentication.getName());  // 인증에 성공한 사용자 이름 출력
+                        httpServletResponse.sendRedirect("/");  // root 페이지로 이동
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override  //AuthenticationException : 인증 예외 객체를 받음
+                    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+                        System.out.println("exception : " + e.getMessage());
+                        httpServletResponse.sendRedirect("/login");
+                    }
+                })
+                // /loginPage 이 경로로 접근하는 사용자는 인증을 받지 않아도 이 페이지에 접근이 가능하도록 설정
+                .permitAll();
     }
 }
